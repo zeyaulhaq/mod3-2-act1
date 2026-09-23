c7ee7323ba50f51368657dbf99b994c426520aca
zeyaulhaq <rastamarly@gmail.com>
2026-09-23 22:20:20 +0800

    checkov suppressed

--- main.tf
+++ main.tf
@@ -18,5 +18,12 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "workshop" {
+  #checkov:skip=CKV2_AWS_62: Ensure S3 buckets should have event notifications enabled
+  #checkov:skip=CKV_AWS_21: Ensure all data stored in the S3 bucket have versioning enabled
+  #checkov:skip=CKV2_AWS_6: Ensure that S3 bucket has a Public Access block
+  #checkov:skip=CKV_AWS_145: Ensure that S3 buckets are encrypted with KMS by default
+  #checkov:skip=CKV_AWS_18: Ensure the S3 bucket has access logging enabled
+  #checkov:skip=CKV_AWS_144: Ensure that S3 bucket has cross-region replication enabled
+  #checkov:skip=CKV2_AWS_61: Ensure that an S3 bucket has a lifecycle configuration
   bucket_prefix = "zeya-state-bucket"
 }
