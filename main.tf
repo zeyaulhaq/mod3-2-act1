25d1f7921b5e01e90c948a28e6f256e94b1cac29
zeyaulhaq <rastamarly@gmail.com>
2026-09-23 22:02:57 +0800

    tflint

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_version = ">= 1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
   backend "s3" {
     bucket = "zeya-state-bucket"
     key    = "zeya-state-key.tfstate"
